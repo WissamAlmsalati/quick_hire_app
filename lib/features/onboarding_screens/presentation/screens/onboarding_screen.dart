07166642a75691e1b2266f23149f7c864dccb83f
Tyler34Durden <mohey.mahmoud21@gmail.com>
2024-10-03 14:08:58 +0300

    Resolved merge conflict in main.dart

---
+++ lib/features/onboarding_screens/presentation/screens/onboarding_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:quick_hire/core/utils/constants.dart';
+import 'package:quick_hire/core/widgets/custom_button.dart';
+import 'package:quick_hire/features/authintication_screens/login_Screen.dart';
+import 'package:quick_hire/features/authintication_screens/presentation/screens/sign_up_screen.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor, // Set the background color here
+
+      body: Center(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Gap(50),
+            Image.asset('assets/images/onboarding.png'),
+            Gap(50),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 13, right: 13),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'QuickHire',
+                      style: Theme.of(context).textTheme.displayLarge,
+
+                    ),
+                    const Text(
+                      'A platform that provides work opportunities for those who wish to work independently, at their own time and location.',
+                      style:  TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                        fontFamily: 'Urbanist',
+                        color: Color(0xFF0077B5),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+                    Gap(30),
+               CustomButton(
+                text: 'Login',
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginScreen()),
+                  );
+                },
+                color: AppColors.primaryColor,
+                textColor: AppColors.backgroundColor,
+              ),
+              Gap(19),
+            CustomButton(
+              text: "Sign up",
+              onPressed: () {},
+              color: AppColors.backgroundColor,
+              textColor: AppColors.primaryColor,
+              isHaveBorder: true,
+              borderColor: AppColors.primaryColor,
+              topPd: MediaQuery.of(context).size.height * 0.005,
+              buttomPd: MediaQuery.of(context).size.height * 0.005,
+            ),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
-import 'package:quick_hire/core/theme/app_theme.dart';
-import 'package:quick_hire/features/authintication_screens/data/repositories/auth_repository.dart';
-import 'package:quick_hire/features/authintication_screens/data/repositories/auth_repository_impl.dart';
-import 'package:quick_hire/features/authintication_screens/presentation/cubit/auth_cubit.dart';
-import 'package:quick_hire/features/authintication_screens/presentation/screens/login_screen.dart';
+import 'core/theme/app_theme.dart';
+import 'features/authintication_screens/presentation/screens/sign_up_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AuthRepository>(
-          create: (_) => AuthRepositoryImpl(),
-        ),
-        BlocProvider<AuthCubit>(
-          create: (context) => AuthCubit(
-            context.read<AuthRepository>(),
-          ),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'My App',
-        theme: AppTheme.getLightTheme(context),
-        home: const LoginScreen(),
-      ),
+    return MaterialApp(
+      title: 'My App',
+      theme: AppTheme.getLightTheme(context),
+      home: const SignUpProvider(),
     );
   }
 }
