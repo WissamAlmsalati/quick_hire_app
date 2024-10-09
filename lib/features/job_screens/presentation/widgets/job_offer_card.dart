8faef5d9365762c26cf4c81c8ff4486ee26fddf1
Tyler34Durden <mohey.mahmoud21@gmail.com>
2024-10-09 18:23:54 +0300

    add icons from mohey

---
+++ lib/features/job_screens/presentation/widgets/job_offer_card.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quick_hire/core/utils/constants.dart';
+
+class JobOfferCard extends StatelessWidget {
+  final String jobTitle;
+  final String jobDescription;
+  final String budgetMax;
+  final String budgetMin;
+  const JobOfferCard({super.key, required this.jobTitle, required this.jobDescription, required this.budgetMax, required this.budgetMin});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          jobTitle,
+          style: Theme.of(context).textTheme.displaySmall,
+        ),
+        SizedBox(height: 10),
+        Text(
+          jobDescription,
+          style: TextStyle(
+            color: AppColors.typographyColor,
+          ),
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+
+            SizedBox(width: 5),
+            Text(
+              'Budget: ',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              budgetMin + ' - ' + budgetMax,
+              style: TextStyle(
+                color: AppColors.typographyColor,
+              ),
+            ),
+            Spacer(),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                backgroundColor: AppColors.primaryColor, // Set background color
+                side: BorderSide(
+                  color: AppColors.primaryColor,
+                  width: 1.5,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.05, vertical: MediaQuery.of(context).size.width * 0.001), // Add internal padding
+                child: Text(
+                  "View More",
+                  style: TextStyle(
+                    color: AppColors.backgroundColor,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],);
+  }
+}
