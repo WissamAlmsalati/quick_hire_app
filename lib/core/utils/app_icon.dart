690c4b920a488c8696ae239c29099e51474e37e0
Tyler34Durden <mohey.mahmoud21@gmail.com>
2024-10-10 09:52:13 +0300

    added skillbuttons to freelancer

--- lib/core/utils/app_icon.dart
+++ lib/core/utils/app_icon.dart
@@ -2,5 +2,27 @@ class AppIcons{
   static const String ic_launcher = 'assets/icons/ic_launcher.png';
   static const String facebookIcon = "assets/icons/facebook.svg";
   static const String googleIcon = "assets/icons/google-icon-logo.svg";
-
+  static const String settingIcon = "assets/icons/settings.svg";
+  static const String homeIcon = "assets/icons/home-04.svg";
+  static const String editIcon = "assets/icons/edit-contained.svg";
+  static const String jobsIcon = "assets/icons/icon-brief-search.svg";
+  static const String inboxIcon = "assets/icons/inbox-02.svg";
+  static const String codeIcon = "assets/icons/code.svg";
+  static const String coinIcon = "assets/icons/currency-coin-dollar.svg";
+  static const String locationIcon = "assets/icons/marker-02.svg";
+  static const String paintBoard = "assets/icons/paint board.svg";
+  static const String videoPlayerIcon = "assets/icons/video-play.svg";
+  static const String backIcon = "assets/icons/chevron-left.svg";
+  static const String musicIcon = "assets/icons/music.svg";
+  static const String wrenchIcon = "assets/icons/wrench.svg";
+  static const String cameraIcon = "assets/icons/camera 01.svg";
+  static const String checkIcon = "assets/icons/check-contained.svg";
+  static const String circleIcon = "assets/icons/circle.svg";
+  static const String cpuIcon = "assets/icons/cpu.svg";
+  static const String searchIcon = "assets/icons/search-01.svg";
+  static const String penIcon = "assets/icons/fountain pen.svg";
+  static const String notesIcon = "assets/icons/notes 01.svg";
+  static const String xCircleIcon = "assets/icons/x-circle-contained.svg";
+  static const String speakerIcon = "assets/icons/announcement.svg";
+  static const String userIcon = "assets/icons/user-profile-02.svg";
 }
